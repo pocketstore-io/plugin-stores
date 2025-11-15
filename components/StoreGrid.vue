5d4b04284d51c44f24fe9209a0bdc1df8a80ce9e
jonathan-martz <github@jmartz.de>
2025-11-15 22:37:46 +0100

    work on cotennt

--- components/StoreGrid.vue
+++ components/StoreGrid.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="bg-white px-3 py-3">
     <section class="grid grid-cols-6 gap-3">
-      <div v-for="item in 6" class="col-span-6 md:col-span-2">
+      <div v-for="item in 3" class="col-span-6 md:col-span-2">
         <StoreCard />
       </div>
     </section>
@@ -9,4 +9,5 @@
 </template>
 
 <script setup lang="ts">
+import StoreCard from "./StoreCard.vue";
 </script>
